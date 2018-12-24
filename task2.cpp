8010d15e834cbdb8985450f48833f7b34818ceb7
llOzzeell <maksim.nemna.dev@gmail.com>
2018-12-24 16:02:46 +0300

    fix 3

--- task2.cpp
+++ task2.cpp
@@ -5,22 +5,16 @@ using namespace std;
 
 bool isPalindrom(string s){
 
-    // в этом ифе как раз и проверяется что строка не пустая
-    // возможно вы не увидели вчера эту строчку, или я слишком туп и не понимаю чего-то))
-	if(s.length() > 0)
+	for(unsigned int i = 0; i < s.length()/2; i++)
 	{
-		for(unsigned int i = 0; i < s.length()/2; i++)
-		{
-            if(s[i] != s[s.length()-1 - i]) return false;
-		}
-		return true;
+        if(s[i] != s[s.length()-1 - i]) return false;
 	}
-	else return false;
+	return true;
 }
 
 int main(int argc, char **argv)
 {
-	cout << isPalindrom("abba");
+	cout << isPalindrom("");
 	return 0;
 }
 
